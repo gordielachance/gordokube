8b1c47aa8d3bfffbfd6b6794b248949b3cb92d11
gordielachance <gordie.lachance@gmail.com>
2018-01-26 00:18:39 +0100

    Loops 'n Slides Loop: Co-workers

---
+++ loops-n-slides/loop-coworkers.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Loops 'n Slides Loop: Co-workers
+ *
+ * Display a post title and excerpt
+ */
+global $loopsns_loop;
+?>
+
+<?php if ( have_posts() ){ ?>
+    <?php 
+    while( have_posts() ){
+        the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            
+            <h3 class="loopsns-slide-title">
+                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+            </h3>
+            <div class="gordokube-trombinou"></div>
+            <?php the_excerpt(); ?>
+        </article>
+
+   <?php 
+    }
+
+}
+?>
+TOTO
